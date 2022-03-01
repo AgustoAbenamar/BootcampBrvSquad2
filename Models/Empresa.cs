b03d8497c24a2ddfeecc268b8d18b6570ecd2f7d
AgustoAbenamar <neneoxaugusto@gmail.com>
2022-03-01 11:04:16 -0600

    Create Empresa.cs

---
+++ Models/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Model
+{
+    public class Empresa
+    {
+        [Key]
+        public int EmpresaId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public ICollection<Vacante> Vacantes { get; set; }
+    }
+}
