a10f5dd34c63e81608588e85505e831143fcb22a
AgustoAbenamar <neneoxaugusto@gmail.com>
2022-03-01 11:04:19 -0600

    Create Usuario.cs

---
+++ Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Model
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Repassword { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int Activo { get; set; }
+
+    }
+}
