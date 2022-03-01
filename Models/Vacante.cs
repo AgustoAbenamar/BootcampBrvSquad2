0d35cacce4673bb0d429cd81c20e7e5f20f9671e
AgustoAbenamar <neneoxaugusto@gmail.com>
2022-03-01 11:04:23 -0600

    Create Vacante.cs

---
+++ Models/Vacante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Model
+{
+    public class Vacante
+    {        
+        [Key]
+        public int VacanteId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public Empresa Empresa { get; set; }
+    }
+}
